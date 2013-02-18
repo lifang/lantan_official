4bc9071e630a3cd1ae8e03840258eb91447e5838
lujian <suzhou_it_boy@163.com>
2013-02-18 10:01:52 +0800

    lujian

--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -1,13 +1,27 @@
 #encoding: utf-8
 #官网客户个人信息控制器
 class UserInfoController < ApplicationController
-  #储值卡收支记录
+  #储值卡/打折卡收支记录
   def svcard_records
+    current_user = Customer.find(session[:customer_id])
+    
+    @records = current_user.c_svc_relations[0].svcard_use_records  #储值卡记录信息
+    
+    #types => 1 为  储值卡
+    
+    #types => 2 为  打折卡
     
   end
   
   #消费账单
   def con_records
+    @all = Order.find(:all,
+      :conditions => ["c_pcard_relation_id = ? and c_svc_relation_id = ? and status = ? ",0,0,1]) #消费订单记录
     
+    
+    
+    
+    
+     
   end
 end
