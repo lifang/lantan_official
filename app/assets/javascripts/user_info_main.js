7cbe549d77b2798e3e0bae9b871ee408b76007bd
lujian <suzhou_it_boy@163.com>
2013-02-19 10:19:14 +0800

    lujian

---
+++ app/assets/javascripts/user_info_main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+ 
+//偶数行变色
+$(function(){
+    $(".data_table > tbody > tr:odd").addClass("tbg");
+    $(".data_tab_table > tbody > tr:odd").addClass("tbg");
+});
+
+//点击单选框
+$("#search_btn").click(function (){
+   
+    var time_value= $('input:radio[name="time"]:checked').val();
+    var is_billing = $("#is_billing").attr("value");
+    location.href = "/user_info/con_records?time="+time_value+"&is_billing="+is_billing
+});
--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -4,24 +4,48 @@ class UserInfoController < ApplicationController
   #储值卡/打折卡收支记录
   def svcard_records
     current_user = Customer.find(session[:customer_id])
-    
-    @records = current_user.c_svc_relations[0].svcard_use_records  #储值卡记录信息
-    
-    #types => 1 为  储值卡
-    
-    #types => 2 为  打折卡
-    
+    @records = current_user.c_svc_relations[0].svcard_use_records.paginate(
+      :page => params[:page],:per_page => 6)  #当前用户所有储值卡记录信息
   end
   
   #消费账单
   def con_records
-    @all = Order.find(:all,
-      :conditions => ["c_pcard_relation_id = ? and c_svc_relation_id = ? and status = ? ",0,0,1]) #消费订单记录
-    
-    
-    
-    
-    
-     
+    @orders = Order.find(:all,
+      :conditions => ["c_pcard_relation_id = ? and c_svc_relation_id = ? and status = ? ",0,0,1]).paginate(
+      :page => params[:page],:per_page => 5) #消费订单记录
+  end
+
+  #按要求查找记录
+  def search_records
+    time = params[:int]  #判断选择情况
+    is_billing = params[:is_billing]
+    @records = search_records(time,is_billing)
+    render "/user_info/con_records"
+  end
+  
+  private
+  
+  def search_records(time,is_billing) #查找相应记录
+    if is_billing == 0  #未开出
+      if time == 0  #当前月
+        Order.find(:all, :conditions => ["month(created_at) = month(now()) and status = ",0])
+      elsif time == 1 #近二月
+      elsif time == 2 #近三月
+      elsif time == 3 #全部
+      end
+    elsif is_billing == 1 #已开出
+      if time == 0  #当前月
+      elsif time == 1 #近二月
+      elsif time == 2 #近三月
+      elsif time == 3 #全部
+      end
+    elsif is_billing == 2 #已寄出
+      if time == 0  #当前月
+      elsif time == 1 #近二月
+      elsif time == 2 #近三月
+      elsif time == 3 #全部
+      end
+    end
+    Order.find(:all, :conditions => ["month(created_at) = month(now()) and status = ",0])
   end
 end
