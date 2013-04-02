dec84087187d98bb54f153f8fd7b309bbf89dcb3
lifang <santhe_2004@126.com>
2013-04-02 17:24:08 +0800

    update style

--- app/assets/javascripts/regist_validate.js
+++ app/assets/javascripts/regist_validate.js
@@ -17,3 +17,19 @@ function regist_validate(){
     }
     return true;
 }
+
+function pop_xieyi(){
+    alert("test");
+    var scolltop = document.body.scrollTop|document.documentElement.scrollTop;
+    var win_height = document.documentElement.clientHeight;//jQuery(document).height();
+    var z_layer_height = $("#xieyi_div").height();
+    $("#xieyi_div").css('top',(win_height-z_layer_height)/2 + scolltop);
+
+    var doc_width = $(document).width();
+    var layer_width = $("#xieyi_div").width();
+    $("#xieyi_div").css('left',(doc_width-layer_width)/2);
+    $("#xieyi_div").css('display','block');
+    $("#xieyi_div .close").click(function(){
+        $("#xieyi_div").css('display','none');
+    })
+}
