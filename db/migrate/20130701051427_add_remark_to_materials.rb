ef6dc68171073b24cb8317ad3368a812a8d807ea
lifang <santhe_2004@126.com>
2013-04-03 17:49:33 +0800

    add migrate

--- db/migrate/20130701051427_add_remark_to_materials.rb
+++ db/migrate/20130701051427_add_remark_to_materials.rb
@@ -1,5 +1,6 @@
 class AddRemarkToMaterials < ActiveRecord::Migration
   def change
-    add_column :materials, :remark, :string
+    add_column :materials, :remark, :string, :limit => 1000
+    add_column :materials, :check_num, :integer
   end
 end
