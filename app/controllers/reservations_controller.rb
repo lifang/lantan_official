0f4a5fc84e858d382b9b50860265b49351471eea
cloud <cloud@cloud-Sony.(none)>
2013-04-09 13:31:11 +0800

    预约功能增加了小时和分钟

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,8 +12,9 @@ class ReservationsController < ApplicationController  #预约
       @car_num = CarNum.find_or_create_by_num(params[:car_number].strip)
       @customer = Customer.find_or_create_by_name_and_mobilephone(params[:customer_name].strip, params[:telephone].strip)
       @customer_num_relation = CustomerNumRelation.find_or_create_by_customer_id_and_car_num_id(@customer.id, @car_num.id)
+      res_time = params[:res_day].strip+" "+params[:res_hour]+":"+params[:res_mins]+":00"
       @reservation = Reservation.create(:car_num_id => @car_num.id,
-        :res_time => params[:res_time].strip, :store_id => @store.id, :status => Reservation::STATUS[:NOMAL])
+        :res_time => res_time, :store_id => @store.id, :status => Reservation::STATUS[:NOMAL])
       params[:select_service].each do |s|
         @res_prod_relation = ResProdRelation.create(:product_id => s, :reservation_id => @reservation.id)
       end
