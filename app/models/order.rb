4cd1fe486feccfd6d696e8b3d51553a804cadd48
孟凡玲 <mengfanling825@163.com>
2013-04-11 09:11:32 +0800

    add order status constat

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,8 +7,9 @@ class Order < ActiveRecord::Base
   belongs_to :car_num
 
   IS_BILLING = {:ONE => 1,:TWO => 2,:THREE => 3}
-  
-  STATUS = {:NOMAL => 0, :DELETE => 1}
+  STATUS = {:NORMAL => 0, :SERVICING => 1, :WAIT_PAYMENT => 2, :BEEN_PAYMENT => 3, :FINISHED => 4, :DELETED => 5, :INNORMAL => 6}
+  #0 正常未进行  1 服务中  2 等待付款  3 已经付款  4 已结束  5已删除  6未分配工位
+ 
   USER_INFO_PER_PAGE = 3
 
   def billing_status
