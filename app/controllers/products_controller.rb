a1e3dca2429ff386c9ffa0433eb0fb93ef328c57
lifang <santhe_2004@126.com>
2013-04-23 17:25:20 +0800

    update detial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController    #产品与服务
   def index     #产品&服务
     @store = SStore.find_by_id(params[:store_id].to_i)
     all_products = SProduct.find(:all, :select => "id, name, types, img_url, is_service",
-      :conditions => ["store_id = ?", params[:store_id].to_i]).group_by{ |t| t.is_service }
+      :conditions => ["status = ? and store_id = ?", Product::STATUS[:NOMAL], params[:store_id].to_i]).group_by{ |t| t.is_service }
     @services = all_products[SProduct::IS_SERVICE_VALUE[:YES]].group_by{ |t| t.types } if all_products and
       all_products[SProduct::IS_SERVICE_VALUE[:YES]]
     @products = all_products[SProduct::IS_SERVICE_VALUE[:NO]] if all_products
