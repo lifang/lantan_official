61d8afa08d8097c35fede05e448dd093e2b3c943
lifang <santhe_2004@126.com>
2013-04-23 14:58:30 +0800

    update detial

--- app/helpers/constant.rb
+++ app/helpers/constant.rb
@@ -19,7 +19,7 @@ module Constant
   PASSWORD = "123456"
 
   #上传图片的比例
-  SALE_PICSIZE =[300,230,663]
-  P_PICSIZE = [50,154,246,300,356]
-  C_PICSIZE = [148,154]
+  SALE_PICSIZE =[300,230,663] #活动图片比例
+  P_PICSIZE = [50,154,246,300,356] #产品图片比例
+  C_PICSIZE = [148,154]  #储值卡图片
 end
