4105890ea3faa815d2be87dd1f5ba90c77a325f8
Lup <165340815@qq.com>
2013-05-17 14:15:34 +0800

    wrong name

--- db/migrate/20130801051422_add_updated_at_to_c_svc_relations.rb
+++ db/migrate/20130801051422_add_updated_at_to_c_svc_relations.rb
@@ -1,10 +1,10 @@
 class AddUpdatedAtToCSvcRelations < ActiveRecord::Migration
   def change
-    add_column :c_svc_realtions, :updated_at, :datetime
+    add_column :c_svc_relations, :updated_at, :datetime
     add_column :svcard_prod_relations, :created_at, :datetime
     add_column :svcard_prod_relations, :updated_at, :datetime
 
-    add_index :c_svc_realtions, :updated_at
+    add_index :c_svc_relations, :updated_at
     add_index :svcard_prod_relations, :created_at
     add_index :svcard_prod_relations, :updated_at
   end
