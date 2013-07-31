37b4a95d1b83e72d0fa1e99de5ed1c754e48d3e3
cloud <cloud@cloud-Sony.(none)>
2013-07-31 16:44:15 +0800

    modify bug

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -1,6 +1,7 @@
 module HomepageHelper
  def province#省份
-   City.find_by_sql("select * from cities where parent_id = 0")
+   #City.find_by_sql("select * from cities where parent_id = 0")
+   City.where("parent_id = 0")
  end
 
  def current_customer
